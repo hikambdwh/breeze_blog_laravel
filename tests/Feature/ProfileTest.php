299cb9c807de36bad5295726f88af74e9a136142
hikamBaidawih <hikambaidawih@gmail.com>
2026-06-16 21:34:54 +0700

    testing unit

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -19,7 +19,8 @@ test('profile information can be updated', function () {
         ->actingAs($user)
         ->patch('/profile', [
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'username' => 'testuserxxx',
+            'email' => 'test@gmail.com',
         ]);
 
     $response
@@ -29,7 +30,7 @@ test('profile information can be updated', function () {
     $user->refresh();
 
     $this->assertSame('Test User', $user->name);
-    $this->assertSame('test@example.com', $user->email);
+    $this->assertSame('test@gmail.com', $user->email);
     $this->assertNull($user->email_verified_at);
 });
 
@@ -40,6 +41,7 @@ test('email verification status is unchanged when the email address is unchanged
         ->actingAs($user)
         ->patch('/profile', [
             'name' => 'Test User',
+            'username' => 'testuserxxx',
             'email' => $user->email,
         ]);
 
